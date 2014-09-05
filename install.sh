df18a1b565128056139444ca8afe4d9457cc48f1
dmccarthy <dmccarthy@tssg.org>
2014-09-05 19:27:03 +0100

    Fixed script

--- install.sh
+++ install.sh
@@ -342,7 +342,7 @@ tmux attach -t \$SESSION
 
 DELIM
 
- 
+
 
 cat > /home/ubuntu/generate_api_clients.sh <<DELIM
 #!/bin/bash
@@ -351,7 +351,7 @@ cd /home/ubuntu/repos/openi_android_sdk
 bash build-cloudlet-sdk.sh \$1
 bash build-graph-api-sdk.sh \$1
 bash build-android-sdk.sh \$1
-bash build-android-sdk.sh \$1
+bash build-auth-sdk.sh \$1
 
 cp openi-cloudlet-android-sdk-1.0.0.jar  /home/ubuntu/repos/mongrel2/static/android-sdk/
 cp openi-graph-api-android-sdk-1.0.0.jar /home/ubuntu/repos/mongrel2/static/android-sdk/
