317c937ea8154e36b1c1644568e7d92bfab8ea3f
Philip O'Brien <obrien.philip.p@gmail.com>
2014-10-15 11:26:50 +0000

    add proper python path for api-framework

--- utils/tmux_openi.sh
+++ utils/tmux_openi.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 SESSION="OPENi"
 
 tmux has-session -t $SESSION
@@ -20,7 +22,7 @@ tmux new-window    -t $SESSION -a -n    'Auth framework'
 
 tmux send-keys -t $SESSION:1 ' cd ~/repos/mongrel2/ && sh start_mongrel2.sh'  Enter
 tmux send-keys -t $SESSION:2 ' cd ~/repos/cloudlet/ && node main.js'  Enter
-tmux send-keys -t $SESSION:3 ' cd ~/repos/api-framework/OPENiapp/ && python manage.py runserver 0.0.0.0:8889' Enter
+tmux send-keys -t $SESSION:3 ' cd ~/repos/api-framework/OPENiapp/ && venv/bin/python manage.py runserver 0.0.0.0:8889' Enter
 tmux send-keys -t $SESSION:4 ' cd ~/repos/uaa/proxy && node proxy.js ' Enter
 tmux select-window -t :0
 
