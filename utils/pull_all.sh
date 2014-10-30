79d0d85f373f0f01cb204ac0bd4b3ba2a47a0a0a
openi-fokus <openi@fokus.fraunhofer.de>
2014-10-30 18:01:31 +0100

    removed cloudlet

--- utils/pull_all.sh
+++ utils/pull_all.sh
@@ -17,5 +17,4 @@ echo api-builder        && cd api-builder       && git pull ; cd ../
 echo api-framework      && cd api-framework     && git pull ; cd ../
 echo openi_android_sdk  && cd openi_android_sdk && git pull ; cd ../
 echo api-builder        && cd api-builder       && git pull ; cd ../
-echo cloudlet           && cd cloudlet          && git pull ; cd ../
 echo uaa                && cd uaa               && git pull ; cd ../
