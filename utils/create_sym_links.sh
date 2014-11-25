a40d44a222dac2731d5a94b725196f8cf56ed52b
Philip O'Brien <obrien.philip.p@gmail.com>
2014-11-25 12:06:53 +0000

    trailing slashes in paths caused link dirs to be deleted

--- utils/create_sym_links.sh
+++ utils/create_sym_links.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -fr ~/repos/cloudlet-platform/node_modules/DAO/
-rm -fr ~/repos/cloudlet-platform/node_modules/cloudlet_api/
+rm -fr ~/repos/cloudlet-platform/node_modules/DAO
+rm -fr ~/repos/cloudlet-platform/node_modules/cloudlet_api
 rm -fr ~/repos/cloudlet-platform/node_modules/object_api
 rm -fr ~/repos/cloudlet-platform/node_modules/swagger-def
 rm -fr ~/repos/cloudlet-platform/node_modules/type_api
