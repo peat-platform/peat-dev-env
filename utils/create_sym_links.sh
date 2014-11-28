962889f851361708e1c0b6f908f59fee7f5a10fc
dmccarthy <dmccarthy@tssg.org>
2014-11-28 18:02:43 +0000

    Added permissions backend and refactored couchbase client code

--- utils/create_sym_links.sh
+++ utils/create_sym_links.sh
@@ -2,13 +2,17 @@
 rm -fr ~/repos/cloudlet-platform/node_modules/DAO/
 rm -fr ~/repos/cloudlet-platform/node_modules/cloudlet_api/
 rm -fr ~/repos/cloudlet-platform/node_modules/object_api
+rm -fr ~/repos/cloudlet-platform/node_modules/search_api
 rm -fr ~/repos/cloudlet-platform/node_modules/swagger-def
 rm -fr ~/repos/cloudlet-platform/node_modules/type_api
-rm -fr ~/repos/cloudlet-platform/node_modules/notification
+rm -fr ~/repos/cloudlet-platform/node_modules/notifications
+rm -fr ~/repos/cloudlet-platform/node_modules/permissions_api
 
-ln -s ~/repos/dao/           ~/repos/cloudlet-platform/node_modules/DAO
-ln -s ~/repos/cloudlet-api/  ~/repos/cloudlet-platform/node_modules/cloudlet_api
-ln -s ~/repos/object-api/    ~/repos/cloudlet-platform/node_modules/object_api
-ln -s ~/repos/swagger-def/   ~/repos/cloudlet-platform/node_modules/swagger-def
-ln -s ~/repos/type-api/      ~/repos/cloudlet-platform/node_modules/type_api
-ln -s ~/repos/notifications/ ~/repos/cloudlet-platform/node_modules/notifications
+ln -s ~/repos/dao/             ~/repos/cloudlet-platform/node_modules/DAO
+ln -s ~/repos/cloudlet-api/    ~/repos/cloudlet-platform/node_modules/cloudlet_api
+ln -s ~/repos/object-api/      ~/repos/cloudlet-platform/node_modules/object_api
+ln -s ~/repos/search-api/      ~/repos/cloudlet-platform/node_modules/search_api
+ln -s ~/repos/swagger-def/     ~/repos/cloudlet-platform/node_modules/swagger-def
+ln -s ~/repos/type-api/        ~/repos/cloudlet-platform/node_modules/type_api
+ln -s ~/repos/notifications/   ~/repos/cloudlet-platform/node_modules/notifications
+ln -s ~/repos/permissions-api/ ~/repos/cloudlet-platform/node_modules/permissions_api
