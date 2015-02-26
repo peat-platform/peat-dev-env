a3271069d92735b50bea7a6569c56897f07c2aa4
Funi1234 <dconway@tssg.org>
2015-02-26 17:01:37 +0000

    Updated N1QL run command

--- utils/tmux_openi.sh
+++ utils/tmux_openi.sh
@@ -25,7 +25,7 @@ tmux new-window    -t $SESSION -a -n    'Admin Dashboard'
 tmux send-keys -t $SESSION:1 ' cd ~/repos/mongrel2/ && sh start_mongrel2.sh'  Enter
 tmux send-keys -t $SESSION:2 ' cd ~/repos/cloudlet-platform/ && node lib/main.js'  Enter
 tmux send-keys -t $SESSION:3 ' cd ~/repos/api-framework/OPENiapp/ && venv/bin/python manage.py runserver 0.0.0.0:8889' Enter
-tmux send-keys -t $SESSION:4 ' /opt/n1ql/cbq-engine -couchbase http://localhost:8091/'  Enter
+tmux send-keys -t $SESSION:4 ' /opt/n1ql/cbq-engine -datastore=http://localhost:8091/'  Enter
 tmux send-keys -t $SESSION:5 ' cd ~/repos/openi-auth-dialogs/ && node bin/www'  Enter
 tmux send-keys -t $SESSION:6 ' cd ~/repos/admin-dashboard/    && node bin/www'  Enter
 
