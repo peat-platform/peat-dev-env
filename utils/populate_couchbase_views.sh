6b051989a7240760a623e59fae7b6f0a0022accd
dmccarthy <dmccarthy@tssg.org>
2015-03-05 10:37:56 +0000

    Got permissions working end to end

--- utils/populate_couchbase_views.sh
+++ utils/populate_couchbase_views.sh
@@ -76,3 +76,18 @@ curl --cacert $SSL_CERT -X PUT \
       }
    }' \
   http://admin:password@localhost:8092/clients/_design/clients_views
+
+
+
+    curl   -X PUT \
+    -H "Accept:application/json" \
+    -H "Content-Type: application/json" \
+    -d '{
+        "views": {
+           "app_permissions": {
+              "map": "function (doc, meta) {\n splitId = meta.id.split(\"_\")\n emit([splitId[0], splitId[1]], doc)\n}",
+              "reduce":"_count"
+           }
+        }
+     }' \
+    http://admin:password@localhost:8092/app_permissions/_design/permission_views
