b9e7fa9e940debaea7e4c6576e3f3e379480d2c2
dmccarthy <dmccarthy@tssg.org>
2015-04-30 00:24:53 +0100

    removed unrequired jars

--- utils/generate_api_clients.sh
+++ utils/generate_api_clients.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 cd ~/repos/openi_android_sdk
 
-bash build-cloudlet-sdk.sh $1
-bash build-graph-api-sdk.sh $1
 bash build-android-sdk.sh $1
-bash build-auth-sdk.sh $1
 
-cp openi-cloudlet-android-sdk-1.0.0.jar  ~/repos/mongrel2/static/android-sdk/
-cp openi-graph-api-android-sdk-1.0.0.jar ~/repos/mongrel2/static/android-sdk/
-cp openi-android-sdk-1.0.0.jar           ~/repos/mongrel2/static/android-sdk/
-cp openi-auth-android-sdk-1.0.0.jar      ~/repos/mongrel2/static/android-sdk/
 cp openi-client-lib.aar                  ~/repos/mongrel2/static/android-sdk/
