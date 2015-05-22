d92ddc8cb1eafe35a83c1ea3752b678abd777616
dmccarthy <dmccarthy@tssg.org>
2015-05-22 11:27:07 +0100

    updated types

--- utils/npm_all.sh
+++ utils/npm_all.sh
@@ -2,19 +2,18 @@
 
 cd ~/repos
 
-echo cloudlet-platform  && cd cloudlet-platform && npm install --no-bin-links ; cd ../
-echo cloudlet-api       && cd cloudlet-api      && npm install --no-bin-links ; cd ../
-echo object-api         && cd object-api        && npm install --no-bin-links ; cd ../
-echo type-api           && cd type-api          && npm install --no-bin-links ; cd ../
-echo m2nodehandler      && cd m2nodehandler     && npm install --no-bin-links ; cd ../
-echo dao                && cd dao               && npm install --no-bin-links ; cd ../
-echo swagger-def        && cd swagger-def       && npm install --no-bin-links ; cd ../
-echo mongrel2           && cd mongrel2          && cd ../
-echo dbc                && cd dbc               && npm install --no-bin-links ; cd ../
-echo cloudlet-utils     && cd cloudlet-utils    && npm install --no-bin-links ; cd ../
-echo openi-logger       && cd openi-logger      && npm install --no-bin-links ; cd ../
-echo api-builder        && cd api-builder       && cd ../
-echo api-framework      && cd api-framework     && cd ../
-echo openi_android_sdk  && cd openi_android_sdk && cd ../
-echo api-builder        && cd api-builder       && cd ../
-echo uaa                && cd uaa               && cd ../
+echo cloudlet-platform  && cd ~/repos/cloudlet-platform && npm install --no-bin-links ; cd ../
+echo cloudlet-api       && cd ~/repos/cloudlet-api      && npm install --no-bin-links ; cd ../
+echo object-api         && cd ~/repos/object-api        && npm install --no-bin-links ; cd ../
+echo attachment-api     && cd ~/repos/attachment-api    && npm install --no-bin-links ; cd ../
+echo type-api           && cd ~/repos/type-api          && npm install --no-bin-links ; cd ../
+echo search-api         && cd ~/repos/search-api        && npm install --no-bin-links ; cd ../
+echo admin-dashboard    && cd ~/repos/admin-dashboard   && npm install --no-bin-links ; cd ../
+echo user-dashboard     && cd ~/repos/user-dashboard    && npm install --no-bin-links ; cd ../
+echo notifications      && cd ~/repos/notifications     && npm install --no-bin-links ; cd ../
+echo dao                && cd ~/repos/dao               && npm install --no-bin-links ; cd ../
+echo swagger-def        && cd ~/repos/swagger-def       && npm install --no-bin-links ; cd ../
+echo permissions-api    && cd ~/repos/permissions-api   && npm install --no-bin-links ; cd ../
+echo auth-api           && cd ~/repos/auth-api          && npm install --no-bin-links ; cd ../
+echo crud-api           && cd ~/repos/crud-api          && npm install --no-bin-links ; cd ../
+echo /peat-aggregator   && cd ~/repos//peat-aggregator  && npm install --no-bin-links ; cd ../
