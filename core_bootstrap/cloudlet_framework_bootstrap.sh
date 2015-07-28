1664c820031c6b48870731080566c7571c1f2d1b
Dylan Conway <funi1234@gmail.com>
2015-07-28 11:55:49 +0100

    Added Couchbase 4 + indexes on tables
    Updated piwik code from openi repo

--- core_bootstrap/cloudlet_framework_bootstrap.sh
+++ core_bootstrap/cloudlet_framework_bootstrap.sh
@@ -32,42 +32,37 @@ apt-get install -y libsqlite3-dev
 
 # Install Mongrel2
 cd /tmp ;
-git clone https://github.com/peat-platform/mongrel2-server
-#git clone https://www.github.com/aidenkeating/mongrel2.git
-#wget --quiet --no-check-certificate https://github.com/zedshaw/mongrel2/releases/download/v1.9.1/mongrel2-v1.9.1.tar.gz ;
-#tar -xzvf mongrel2-v1.9.1.tar.gz
-cd /tmp/mongrel2-server/;
+git clone https://github.com/zedshaw/mongrel2.git
+cd /tmp/mongrel2
+git checkout release/1.9.2
 
 make clean all
 sudo make install
-sudo chown -R vagrant:vagrant /tmp
+sudo chown -R $USER:$GROUP /tmp
+
+
 # Install Couchbase
 cd /tmp ;
-
-wget --quiet http://packages.couchbase.com/releases/3.0.0/couchbase-server-enterprise_3.0.0-ubuntu12.04_amd64.deb
-sudo dpkg -i couchbase-server-enterprise_3.0.0-ubuntu12.04_amd64.deb
-rm /tmp/couchbase-server-enterprise_3.0.0-ubuntu12.04_amd64.deb
+wget http://latestbuilds.hq.couchbase.com/couchbase-server/sherlock/3133/couchbase-server-enterprise_4.0.0-3133-ubuntu14.04_amd64.deb
+sudo dpkg -i couchbase-server-enterprise_4.0.0-3133-ubuntu14.04_amd64.deb
+rm /tmp/couchbase-server-enterprise_4.0.0-3133-ubuntu14.04_amd64.deb
+sudo chown -R $USER:$GROUP /tmp
 
 /bin/sleep 10
-sudo /opt/couchbase/bin/couchbase-cli cluster-init --cluster=127.0.0.1:8091 --user=admin --password=password --cluster-ramsize=2372
+sudo /opt/couchbase/bin/couchbase-cli cluster-init --cluster=127.0.0.1:8091 --user=admin --password=password --cluster-ramsize=2372 --services="data;index;query"
 sudo /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=objects     --bucket-type=couchbase --bucket-ramsize=100 --bucket-replica=0 -u admin -p password
 sudo /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=types       --bucket-type=couchbase --bucket-ramsize=100 --bucket-replica=0 -u admin -p password
 sudo /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=attachments --bucket-type=couchbase --bucket-ramsize=100 --bucket-replica=0 -u admin -p password
 sudo /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=permissions --bucket-type=couchbase --bucket-ramsize=100 --bucket-replica=0 -u admin -p password
 sudo /opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 --bucket=app_permissions --bucket-type=couchbase --bucket-ramsize=100 --bucket-replica=0 -u admin -p password
 
-sudo chown -R vagrant:vagrant /tmp
-# Install N1QL DP4
-#sudo mkdir /opt/n1ql
-#sudo chown -R vagrant:vagrant /opt/n1ql
-#cd /opt/n1ql
-#wget --quiet http://packages.couchbase.com/releases/couchbase-query/dp4/couchbase-query_dev_preview4_x86_64_linux.tar.gz
-#tar -xf couchbase-query_dev_preview4_x86_64_linux.tar.gz
-#sudo chown -R vagrant:vagrant /opt/n1ql
-#curl -v http://localhost:8093/query/service -d 'statement=CREATE PRIMARY INDEX ON objects;'
-#curl -v http://localhost:8093/query/service -d 'statement=CREATE PRIMARY INDEX ON types;'
+/bin/sleep 5
+curl -v http://localhost:8093/query/service -d 'statement=CREATE PRIMARY INDEX `objects-index` ON `objects`;'
+curl -v http://localhost:8093/query/service -d 'statement=CREATE PRIMARY INDEX `types-index` ON `types`;'
+curl -v http://localhost:8093/query/service -d 'statement=CREATE PRIMARY INDEX `clients-index` ON `clients`;'
+curl -v http://localhost:8093/query/service -d 'statement=CREATE PRIMARY INDEX `users-index` ON `users`;'
 
-sudo chown -R vagrant:vagrant /tmp
+sudo chown -R $USER:$GROUP /tmp
 
 # Install Elasticsearch & Logstash
 sudo wget -qO - https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
@@ -122,18 +117,27 @@ sudo apt-get update
 sudo apt-get -y install unzip php5 php5-mysql php5-gd
 cd /usr/share
 sudo wget http://builds.piwik.org/latest.zip
-y | sudo unzip latest.zip
+sudo unzip latest.zip
 sudo chmod a+w /usr/share/piwik/tmp
 sudo chmod a+w /usr/share/piwik/config
-sudo chown -R vagrant:vagrant /usr/share/piwik
+sudo chown -R $USER:$GROUP /usr/share/piwik
 
+
+# sudo wget https://debian.piwik.org/repository.gpg -qO piwik-repository.gpg
+# sudo cat piwik-repository.gpg | sudo apt-key add -
+# sudo rm -rf piwik-repository.gpg
+# sudo bash -c 'echo "deb http://debian.piwik.org/ piwik main\ndeb-src http://debian.piwik.org/ piwik main" >> /etc/apt/sources.list.d/piwik.list'
+# sudo apt-get update
+# sudo apt-get install unzip php5-gd -y
+# sudo apt-get install piwik -y
+# sudo chown -R $USER:$GROUP /tmp
 
 cd /usr/share/piwik/plugins
-sudo git clone https://github.com/peat-platform/openi-app-tracker.git OpeniAppTracker
-sudo git clone https://github.com/peat-platform/openi-company-tracker.git OpeniCompanyTracker
-sudo git clone https://github.com/peat-platform/openi-location-tracker.git OpeniLocationTracker
-sudo git clone https://github.com/peat-platform/openi-object-tracker.git OpeniObjectTracker
-sudo chown -R vagrant:vagrant /tmp
+sudo git clone https://github.com/OPENi-ict/openi-app-tracker.git OpeniAppTracker
+sudo git clone https://github.com/OPENi-ict/openi-company-tracker.git OpeniCompanyTracker
+sudo git clone https://github.com/OPENi-ict/openi-location-tracker.git OpeniLocationTracker
+sudo git clone https://github.com/OPENi-ict/openi-object-tracker.git OpeniObjectTracker
+sudo chown -R $USER:$GROUP /tmp
 
 sudo sh -c 'echo "Alias /piwik /usr/share/piwik \n<Directory /usr/share/piwik>\n  Order allow,deny\n  Allow from all\n  AllowOverride None\n  Options Indexes FollowSymLinks\n</Directory>" >> /etc/apache2/apache2.conf'
 sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password password'
@@ -159,4 +163,4 @@ fi
 # mysql -u root -ppassword -e "CREATE USER 'piwik'@'localhost' IDENTIFIED BY 'password'"
 # mysql -u root -ppassword -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES ON piwik.* TO 'piwik'@'localhost'"
 sudo /etc/init.d/apache2 restart
-sudo chown -R vagrant:vagrant /tmp
+sudo chown -R $USER:$GROUP /tmp
