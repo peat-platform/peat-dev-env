ef518831c1c5b97310d5633e19d86b690ab50159
Dylan Conway <funi1234@gmail.com>
2015-08-06 15:25:13 +0100

    Refactored cloudlet provision to reflect changes made to cloudlet platform repo

--- core_provision/cloudlet_framework_provision.sh
+++ core_provision/cloudlet_framework_provision.sh
@@ -5,12 +5,6 @@ cd ~/repos
 
 git clone https://github.com/peat-platform/cloudlet-platform.git
 git clone https://github.com/peat-platform/swagger-def.git
-git clone https://github.com/peat-platform/cloudlet-api.git
-git clone https://github.com/peat-platform/object-api.git
-git clone https://github.com/peat-platform/attachment-api.git
-git clone https://github.com/peat-platform/type-api.git
-git clone https://github.com/peat-platform/search-api.git
-git clone https://github.com/peat-platform/permissions-api.git
 git clone https://github.com/peat-platform/m2nodehandler.git
 git clone https://github.com/peat-platform/dao.git
 git clone https://github.com/peat-platform/mongrel2.git
@@ -24,16 +18,26 @@ git clone https://github.com/peat-platform/user-dashboard.git
 git clone https://github.com/peat-platform/loglet.git
 git clone https://github.com/peat-platform/peat-aggregator.git
 
+mkdir ~/node_modules/admin-dashboard
+ln -s ~/node_modules/admin-dashboard    ~/repos/admin-dashboard/node_modules
+mkdir ~/node_modules/user-dashboard
+ln -s ~/node_modules/user-dashboard     ~/repos/user-dashboard/node_modules
+mkdir ~/node_modules/cloudlet-platform
+ln -s ~/node_modules/cloudlet-platform  ~/repos/cloudlet-platform/node_modules
+mkdir ~/node_modules/swagger-def
+ln -s ~/node_modules/swagger-def        ~/repos/swagger-def/node_modules
+mkdir ~/node_modules/dao
+ln -s ~/node_modules/dao                ~/repos/dao/node_modules
+mkdir ~/node_modules/notifications
+ln -s ~/node_modules/notifications      ~/repos/notifications/node_modules
+mkdir ~/node_modules/peat_rrd
+ln -s ~/node_modules/peat_rrd           ~/repos/peat_rrd/node_modules
+
+
 cd ~/repos/admin-dashboard; npm install --no-bin-links
 cd ~/repos/user-dashboard; npm install --no-bin-links
 cd ~/repos/cloudlet-platform; npm install --no-bin-links
 cd ~/repos/swagger-def; npm install --no-bin-links
-cd ~/repos/cloudlet-api; npm install --no-bin-links
-cd ~/repos/object-api; npm install --no-bin-links
-cd ~/repos/attachment-api; npm install --no-bin-links
-cd ~/repos/type-api; npm install --no-bin-links
-cd ~/repos/search-api; npm install --no-bin-links
-cd ~/repos/permissions-api; npm install --no-bin-links
 cd ~/repos/dao; npm install --no-bin-links
 cd ~/repos/notifications; npm install --no-bin-links
 cd ~/repos/peat-rrd; npm install --no-bin-links
--- core_provision/security_framework_provision.sh
+++ core_provision/security_framework_provision.sh
@@ -2,13 +2,12 @@
 
 cd ~/repos
 
-git clone https://github.com/peat-platform/auth-api.git
-git clone https://github.com/peat-platform/crud-api.git
 git clone https://github.com/peat-platform/auth-dialogs.git
 
+mkdir ~/node_modules/auth-dialogs
+ln -s ~/node_modules/auth-dialogs    ~/repos/auth-dialogs/node_modules
+
 cd ~/repos/auth-dialogs npm install --no-bin-links
-cd ~/repos/auth-api; npm install --no-bin-links
-cd ~/repos/crud-api; npm install --no-bin-links
 
 npm install couchbase
 node /vagrant/core_provision/security_framework_init.js
