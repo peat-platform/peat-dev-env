d6d2bd7f17bb7560d924b5837ce852b7a1691e96
dmccarthy <dmccarthy@tssg.org>
2015-09-02 12:31:05 +0100

    updated repos and specified the develop branches

--- core_provision/cloudlet_framework_provision.sh
+++ core_provision/cloudlet_framework_provision.sh
@@ -3,19 +3,15 @@
 cd ~/repos
 
 
-git clone https://github.com/peat-platform/cloudlet-platform.git
+git clone -b develop https://github.com/peat-platform/cloudlet-platform.git
+git clone -b develop https://github.com/peat-platform/dao.git
+git clone -b develop https://github.com/peat-platform/cloudlet-utils.git
+
 git clone https://github.com/peat-platform/swagger-def.git
 git clone https://github.com/peat-platform/m2nodehandler.git
-git clone https://github.com/peat-platform/dao.git
 git clone https://github.com/peat-platform/mongrel2.git
-git clone https://github.com/peat-platform/dbc.git
-git clone https://github.com/peat-platform/cloudlet-utils.git
-git clone https://github.com/peat-platform/peat-logger.git
-git clone https://github.com/peat-platform/notifications.git
 git clone https://github.com/peat-platform/admin-dashboard.git
 git clone https://github.com/peat-platform/user-dashboard.git
-git clone https://github.com/peat-platform/loglet.git
-git clone https://github.com/peat-platform/peat-aggregator.git
 git clone https://github.com/peat-platform/acceptancetests.git
 
 
@@ -25,7 +21,6 @@ cd ~/repos/cloudlet-platform; npm install --no-bin-links
 cd ~/repos/swagger-def; npm install --no-bin-links
 cd ~/repos/dao; npm install --no-bin-links
 cd ~/repos/notifications; npm install --no-bin-links
-cd ~/repos/peat-rrd; npm install --no-bin-links
 
 
 ln -s /home/vagrant/repos/mongrel2/uploads/ /opt/peat/cloudlet_platform/uploads
--- utils/create_sym_links.sh
+++ utils/create_sym_links.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 rm -fr ~/repos/cloudlet-platform/node_modules/dao
-rm -fr ~/repos/cloudlet-platform/node_modules/cloudlet-api
-rm -fr ~/repos/cloudlet-platform/node_modules/object-api
-rm -fr ~/repos/cloudlet-platform/node_modules/attachment-api
-rm -fr ~/repos/cloudlet-platform/node_modules/search-api
 rm -fr ~/repos/cloudlet-platform/node_modules/swagger-def
-rm -fr ~/repos/cloudlet-platform/node_modules/type-api
-rm -fr ~/repos/cloudlet-platform/node_modules/notifications
-rm -fr ~/repos/cloudlet-platform/node_modules/permissions-api
-rm -fr ~/repos/cloudlet-platform/node_modules/auth-api
-rm -fr ~/repos/cloudlet-platform/node_modules/crud-api
-rm -fr ~/repos/cloudlet-platform/node_modules/peat-aggregator
 
 ln -s ~/repos/dao/             ~/repos/cloudlet-platform/node_modules/dao
-ln -s ~/repos/cloudlet-api/    ~/repos/cloudlet-platform/node_modules/cloudlet-api
-ln -s ~/repos/object-api/      ~/repos/cloudlet-platform/node_modules/object-api
-ln -s ~/repos/attachment-api/      ~/repos/cloudlet-platform/node_modules/attachment-api
-ln -s ~/repos/search-api/      ~/repos/cloudlet-platform/node_modules/search-api
 ln -s ~/repos/swagger-def/     ~/repos/cloudlet-platform/node_modules/swagger-def
-ln -s ~/repos/type-api/        ~/repos/cloudlet-platform/node_modules/type-api
-ln -s ~/repos/notifications/   ~/repos/cloudlet-platform/node_modules/notifications
-ln -s ~/repos/permissions-api/ ~/repos/cloudlet-platform/node_modules/permissions-api
-ln -s ~/repos/auth-api/        ~/repos/cloudlet-platform/node_modules/auth-api
-ln -s ~/repos/crud-api/        ~/repos/cloudlet-platform/node_modules/crud-api
-ln -s ~/repos/peat-aggregator/        ~/repos/cloudlet-platform/node_modules/peat-aggregator
