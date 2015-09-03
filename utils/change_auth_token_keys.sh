64f2b32ae28f05703593b0f9c1cfaa96ba666f2a
dmccarthy <dmccarthy@tssg.org>
2015-09-03 14:53:17 +0100

    Fixed security key override script

--- utils/change_auth_token_keys.sh
+++ utils/change_auth_token_keys.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-openssl genrsa -out new_private_key.pem 2048
-
-openssl rsa -in new_private_key.pem -pubout -out new_public_key.pem
+if [ ! -f new_private_key.pem ]; then
+   openssl genrsa -out new_private_key.pem 2048
+   openssl rsa -in new_private_key.pem -pubout -out new_public_key.pem
+fi
 
 new_private_key=`cat new_private_key.pem`
 new_public_key=`cat new_public_key.pem`
 
-node update_files_with_keys.js "$new_private_key" "$new_public_key" ~/repos/cloudlet-platform/lib/main.js
+node update_files_with_keys.js "$new_private_key" "$new_public_key" ~/repos/cloudlet-platform/lib/config.js
 node update_files_with_keys.js "$new_private_key" "$new_public_key" ~/repos/user-dashboard/libs/config.js
 node update_files_with_keys.js "$new_private_key" "$new_public_key" ~/repos/user-dashboard/app.js
 node update_files_with_keys.js "$new_private_key" "$new_public_key" ~/repos/admin-dashboard/libs/config.js
